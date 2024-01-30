a1805f4dedb08049d3d7c5716c55bb2970fbec99
AbirBal <baltiabir88@gmail.com>
2024-01-30 16:30:12 +0100

    Creation de l'architecture projet

---
+++ Core/Models/Users.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Users
+    {
+        public int IdUser { get; set; }
+        public String Login { get; set; }
+
+        public String Password { get; set; }
+
+
+
+        public Users()
+        {
+
+        }
+
+        public Users (int idUser, string login, string password)
+        {
+            IdUser = idUser;
+            Login = login;
+            Password = password;
+        }
+
+
+        public override string ToString()
+        {
+            return "Id User : " +IdUser+
+                    "Login : " +Login+
+                    "Password : " +Password;
+        }
+    }
+
+
+
+}
---
+++ Fifo/Fifo.HR/Playroll.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Fifo.HR
+{
+    public class Playroll
+    {
+    }
+}
