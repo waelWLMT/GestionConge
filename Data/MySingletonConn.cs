24dec0adfe74a00a6c277e7b6b43f198db0d17e8
unknown <=>
2024-04-28 19:16:05 +0200

    netoyage du code coté access au données et ajout de singleton pattern

---
+++ Data/MySingletonConn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class MySingletonConn
+    {
+        private string _connectionString;
+        public SqlConnection Connection {  get; set; }
+        
+        private static MySingletonConn _instance;
+
+        private MySingletonConn()
+        {
+            this._connectionString = ConfigurationManager.ConnectionStrings["GestConge_dbStr"].ConnectionString;
+            //this._connectionString = "Data Source=.;Initial Catalog=GestionConge;Integrated Security=True;MultipleActiveResultSets=True";
+            this.Connection = new SqlConnection(this._connectionString);
+        }
+
+        public static MySingletonConn getInstance()
+        {
+           if(_instance == null)
+                _instance = new MySingletonConn();
+
+           return _instance;
+
+        }
+
+       
+
+
+    }
+}
